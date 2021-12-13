6f4381ef9a3e761aca14fea9c5e78db6d34bb391
Ethan-Francolla <e.francolla@gmail.com>
2021-12-13 16:35:30 -0500

    added to do list and started instructions-Ethan

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello")
+#####-Imports-#####
+# Import turtle library for drawing and display functions
+import turtle as trtl
+
+# Import random library to utilize random number generation functions
+import random as rand
+
+# Import welcome module storing insturctions and welcome functions
+import welcome as welcome
+
+welcome.welcome_user()
