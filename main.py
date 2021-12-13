5762e2764fd517be483eca83696295ddae860fc1
Ethan-Francolla <e.francolla@gmail.com>
2021-12-13 16:58:09 -0500

    deleted pycache in commit, removed  junk instructions, updated comments, and added instructions to the terminal -Ethan

--- main.py
+++ main.py
@@ -10,3 +10,42 @@ import welcome as welcome
 
 # Call the function to welcome the user to the game
 welcome.welcome_user()
+
+# Define a variable to determine if the user wants to receive debug message in the terminal
+developer_mode = 0
+
+# Ask the user if they want instructions until they enter a valid input
+while True:
+    # Store the user input for instructions request
+    instructions_request = str(input("Please write y for yes, n for no, and d if you would like to enter developer mode: "))
+
+    # Check if the user selected yes and adjust variables appropriately
+    if instructions_request == "y":
+        instructions_request = 1
+        break
+
+    # Check if the user selected no and adjust variables appropriately
+    elif instructions_request == "n":
+        instructions_request = 0
+        break
+
+    # Check if the user wants to enter developer mode
+    elif instructions_request == "d":
+        # Turn on developer mode
+        developer_mode = 1
+        print("You are now in developer mode.")
+
+    # Output an error message if the user enters an invalid input
+    else:
+        print("Sorry! That was not a valid input.\nPlease try again.")
+
+# Print the instructions if the user wants them
+if instructions_request == 1:
+    welcome.print_instructions()
+
+# Don't print the instructions if the user doesn't want them
+elif instructions_request == 0:
+    print("Welcome back!")
+
+# Inform the user the game is starting
+print("Lets start the game!")
--- welcome.py
+++ welcome.py
@@ -5,34 +5,18 @@ def welcome_user():
     # Get the user's name as an input, store it in a variable and then print out a welcome message using that name
     print("-----Welcome to the game!-----\n")
     usr_name = str(input("Please enter your name here: "))
-    print("Welcome to battleship!", usr_name +"!\n")
+    print("Welcome to battleship", usr_name +"!\n")
+    print(usr_name + ",", "would you like to read the instructions for the game?")
 
 
 # Define a function to print the instructions for the game if the user requests them
 def print_instructions():
     # Explain to users some basic info about the game
     print("\n-Game Info-")
-    print("")
-    print("")
 
     #Explain the rules of the game, like how each piece moves
     print("\n-Rules-")
-    print("")
-    print("Each player has to pick one side: either white or black.")
-    print("Each player is allowed 1 legal move per turn with any piece.")
-    print("Pawns can only move forward 1 space, except if it is theirvery first move. If so, they can move forward 2 spaces.")
-    print("It is important to note that when a pawn captures a piece, it can only do so by moving one square accross a diagonal.")
-    print("The last exception/rule regarding pawns is the en-pessant rule.")
-    print("If a player takes their aforementioned first move of 2 spaces,\nanother pawn can take that piece as if it only moved forward 1 square by capturing behind it.")
-    print("Rooks can move upwards and accross the board and can take any piece along their path.")
-    print("Knights can move in L-like shapes moving forward or back 2 squares, and then moving 1 square left or right, taking any piece in their path.")
-    print("Bishops can move along diagonals, taking any piece in their path.")
-    print("Queens are like a combination of rooks and bishops.\n They can move along any diagonal, horizontal, or vertical line, taking any piece in their path.")
-    print("Kings can move 1 space in any direction of the 8 squares that incicle them.")
     
     #Describe the objective of the game
     print("\n-Objective-")
-    print("When another piece threatents to take the king on its very next move, the king is put into check.")
-    print("When the king is put in check, the player put in check must make a legal move to prevent their king from being taken.")
-    print("If there are no legal moves a player can make, and the king is in check then the player is in checkmate and the other player wins!")
-    print("Please note that in chess, a player can never be forced to make an illegal move,\nso if a player can't make a legal mvoe without putting their king into check, it is a stalemate and draw.")
+    
