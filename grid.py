931ccaf29cd6c0e2a4ddffe7c38a7155e0c45b3a
Ethan-Francolla <e.francolla@gmail.com>
2021-12-15 14:38:58 -0500

    added grid -Mikaela & Maria

--- grid.py
+++ grid.py
@@ -14,6 +14,16 @@ label_painter = trtl.Turtle()
 master_painters_list.append(grid_painter)
 master_painters_list.append(label_painter)
 
+# Define variables that will determine the size and shape of the battleship grid
+number_of_sides = 4
+number_of_squares = 64
+squares_per_side = 8
+number_of_squares_on_side = number_of_squares / squares_per_side
+width_of_side = 520
+width_of_square = width_of_side / squares_per_side
+grid_painter_starting_location = -260, -260
+label_painter_starting_location = -228,-295
+
 #####-Functions-#####
 # Define a function that will give the painters the proper attributes
 def set_painter_attributes():
@@ -24,14 +34,112 @@ def set_painter_attributes():
         master_painters_list[i].shape("classic")
         master_painters_list[i].pencolor("black")
         master_painters_list[i].fillcolor("black")
-        master_painters_list[i].speed(5)
+        master_painters_list[i].speed(0)
         master_painters_list[i].shapesize(1)
         master_painters_list[i].setheading(0)
+    
+    # Set individual starting locations
+    grid_painter.goto(grid_painter_starting_location)
+    label_painter.goto(label_painter_starting_location)
+
+# Define a function to draw the borders of the grid
+def draw_borders():
+    print("drawing border")
+    for i in range(3):
+        grid_painter.pendown()
+        grid_painter.forward(width_of_side)
+        grid_painter.left(90)
+
+
+# Define a function to draw the vertical lines
+def draw_vert_lines():
+    print("drawing vert lines")
+    for i in range(4):
+        grid_painter.forward(width_of_side)
+        grid_painter.left(90)
+        grid_painter.forward(width_of_square)
+        grid_painter.left(90)
+        grid_painter.forward(width_of_side)
+        grid_painter.right(90)
+        grid_painter.forward(width_of_square)
+        grid_painter.right(90)
+        
+
+# Define a function to draw the horizontal lines
+def draw_horiz_lines():
+    print("drawing horiz lines")
+    for i in range(4):
+        grid_painter.forward(width_of_square)
+        grid_painter.right(90)
+        grid_painter.forward(width_of_side)
+        grid_painter.left(90)
+        grid_painter.forward(width_of_square)
+        grid_painter.left(90)
+        grid_painter.forward(width_of_side)
+        grid_painter.right(90)
+
+def draw_labels():
+    #Display label drawering turtle
+    label_painter.showturtle()
+
+    #For loop to draw all of the letters and numbers on the board
+    for i in range(number_of_sides):
+        if i < 1:
+            #For loop to label the bottom row of the chessboard with letters
+            for a in range(int(number_of_squares_on_side)): 
+                #Write the appropirate letter from the list of letters with the index of a and move to the next square
+                label_painter.write(grid_letters_list[a], align = "center", font = ("Times New Roman", 20, "normal"))
+                label_painter.forward(width_of_square)
+
+        elif i < 2:    
+            #Move and turn the labeler turtle to the appropriate position
+            label_painter.setheading(180)
+            label_painter.forward(10)
+            label_painter.setheading(90)
+            label_painter.forward(50)
+
+            #For loop to lable the right side of the chessboard with numbers
+            for b in range(int(number_of_squares_on_side)):
+                #Write the appropirate number from the list of numbers with the index of b and move to the next square
+                label_painter.write(grid_numbers_list[b], align = "center", font = ("Times New Roman", 20, "normal"))
+                label_painter.forward(width_of_square)
+
+        elif i < 3:
+            #Move and turn the labeler turtle to the appropriate position
+            label_painter.goto(-228, 260)
+            label_painter.setheading(0)
+
+            #For loop to label the bottom row of the chessboard with letters
+            for c in range(int(number_of_squares_on_side)):
+                #Write the appropirate letter from the list of letters with the index of c and move forward
+                label_painter.write(grid_letters_list[c], align = "center", font = ("Times New Roman", 20, "normal"))
+                label_painter.forward(width_of_square)
+        
+        elif i < 4:
+            #Move and turn the labeler turtle to the appropriate position
+            label_painter.goto(-275, -245)
+            label_painter.setheading(90)
+
+            #For loop to label the bottom row of the chessboard with letters
+            for d in range(int(number_of_squares_on_side)):
+                #Write the appropirate letter from the list of letters with the index of d and move forward
+                label_painter.write(grid_numbers_list[d], align = "center", font = ("Times New Roman", 20, "normal"))
+                label_painter.forward(width_of_square)
+
+    #Ensure the pen is down because the trails will make the board
+    label_painter.pendown()
+
+    #Hide the label drawing turtle when it is done drawing the labels
+    label_painter.hideturtle()
 
 
 # Define a function that will be used to draw the battleship grid
 def draw_grid():
     # Set the attributes of the painters before drawing
     set_painter_attributes()
+    draw_borders()
+    draw_vert_lines()
+    draw_horiz_lines()
+    draw_labels()
 
     
